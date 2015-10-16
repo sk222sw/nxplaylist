d48c79a2b04460101a6cb0491d9a25eec0095c76
Sonny Kjellberg <sk222sw@student.lnu.se>
2015-10-16 21:37:01 +0000

    PlaylistDAL and connection to ClearDB

--- model/DAL/PlaylistDAL.php
+++ model/DAL/PlaylistDAL.php
@@ -6,13 +6,33 @@ require_once("model/Playlist.php");
 class PlaylistDAL {
     
     public function selectAll() {
-        $servername = getenv('IP');
-        $username = getenv('C9_USER');
-        $password = "";
-        $database = "nxplaylist";
-        $dbport = 3306;
+        // $servername = getenv('IP');
+        // $username = getenv('C9_USER');
+        // $password = "";
+        // $database = "nxplaylist";
+        // $dbport = 3306;
         
-        $conn = new \mysqli($servername, $username, $password, $database, $dbport);
+        
+        // $conn = new \mysqli($servername, $username, $password, $database, $dbport);
+        
+        // $DATABASE_URL='mysql://bff0e401b5096a:3ae2ffe2@eu-cdbr-west-01.cleardb.com/heroku_c1e3b5ffbfdbe7f?reconnect=true';
+            
+        // $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        // var_dump($url);
+        // $server = $url["host"];
+        // $username = $url["user"];
+        // $password = $url["pass"];
+        // $db = substr($url["path"], 1);
+        
+        $server = "eu-cdbr-west-01.cleardb.com";
+        $username = "bff0e401b5096a";
+        $password = "3ae2ffe2";
+        $db = "heroku_c1e3b5ffbfdbe7f";
+        
+        $conn = new \mysqli($server, $username, $password, $db);            
+
+        
+
         
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
